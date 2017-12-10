bdff794b04c10ed7d4e91a096e7c4dc3713d7f15
Stephan Griesel <sgriesel@gmail.com>
2017-12-10 16:02:12 +0100

    add javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function clock () {
+    const fullDate = new Date();
+    var hours = fullDate.getHours();
+    var minutes = fullDate.getMinutes();
+    var seconds = fullDate.getSeconds();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = " : " + minutes;
+    document.getElementById('seconds').innerHTML = " : " + seconds;
+}
+
+setInterval(clock,500);
